7578bab6d76fd3b6dd13b6139d7dd80c4c80290a
minjeongJe <zzssjj@naver.com>
2024-07-08 20:39:14 +0900

    modified

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// let news = [];
-// const getLatestNews = async () => {
-//     const url = new URL (`https://eve-the-news-times.netlify.app/top-headlines&country=kr`);
-//     const response = await fetch(url);  
-//     const data = await response.json();
-//     news = data.articles;
-//     console.log("ddd",news);
-// };
-
-// getLatestNews (); 
-
 let news = [];
 const getLatestNews = async () => {
     const url = new URL(`https://eve-the-news-times.netlify.app/top-headlines`);

29e3371a9e29472ab1dfb7ff8019450f8aeed8f7
minjeongJe <zzssjj@naver.com>
2024-07-14 01:10:48 +0900

    Fourth commit

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 let newsList = [];
 const menus = document.querySelectorAll(".menus button, .side-menu-list button");
-menus.forEach(menu => menu.addEventListener("click",(event) => getNewsByCategory(event)))
-
+menus.forEach(menu => menu.addEventListener("click",(event) => getNewsByCategory(event)));
 const searchInput = document.getElementById("search-input");
+
+//pagination
+let totalResults = 0;
+let page = 1;
+const pageSize = 7;
+const groupSize = 5;
 
 const getLatestNews = async (params = {}) => {
     const url = new URL(`https://eve-the-news-times.netlify.app/top-headlines`);
     url.searchParams.set('country', 'kr');
+    url.searchParams.set("page",page); // => $page = page
+    url.searchParams.set("pageSize", pageSize);
 
     for (const [key, value] of Object.entries(params)) {
         url.searchParams.set(key, value);
@@ -27,9 +34,10 @@ const getLatestNews = async (params = {}) => {
                 throw new Error("No result for this search");
             }
             newsList = data.articles;
+            totalResults = data.totalResults
             render(); 
-
-
+            paginationRender();
+ 
         } else {
             throw new Error(data.message || 'Failed to fetch data');
         }
@@ -44,20 +52,19 @@ const getLatestNews = async (params = {}) => {
 };
 
 // 검색 뉴스 가져오기
-const searchNews = () => {
+const searchNews = async() => {
     const keyword = searchInput.value;
-    getLatestNews({ q: keyword });
+    await getLatestNews({ q: keyword });
 
     // 검색 완료 후 검색창 초기화
     searchInput.value = "";
 };
 
 // 카테고리별 뉴스 가져오기 
-const getNewsByCategory = (event) => {
+const getNewsByCategory = async(event) => {
     const category = event.target.textContent.toLowerCase();
-    getLatestNews({ category });
+    await getLatestNews({ category });
 };
-
 
 //UI 그려줌.
 const render = () => {
@@ -79,6 +86,46 @@ const render = () => {
 
   document.getElementById("news-board").innerHTML = newsHTML;
 };
+
+// Pagination
+const paginationRender = () => {
+    // totalResults
+    // page
+    // pageSize
+    // groupSize
+    // totalPages
+    const totalPages = Math.ceil(totalResults / pageSize);
+
+    // pageGroup
+    const pageGroup = Math.ceil(page / groupSize);
+    // lastPage
+    let lastPage = pageGroup * groupSize;
+    // firstPage
+    const firstPage = lastPage - (groupSize - 1) <= 0? 1 : lastPage - (groupSize - 1);
+    // 마지막 페이지그룹이 그룹사이즈보다 작을경우 lastPage = totalPages
+    if (lastPage > totalPages) {
+        lastPage = totalPages;
+    }
+
+    let paginationHTML = `<li class="page-item" ><a class="page-link" href="#">&lt;&lt;</a></li>
+    <li class="page-item" onclick="moveToPage(${page - 1})"><a class="page-link" href="#">&lt;</a></li>`;
+
+    for (let i = firstPage; i <= lastPage; i++) {
+        paginationHTML += `<li class="page-item ${i === page ? 'active' : ''} "onclick="moveToPage(${i})"><a class="page-link" href="#">${i}</a></li>`;
+    }
+
+    paginationHTML +=`<li class="page-item" onclick="moveToPage(${page +1})"><a class="page-link" href="#">&gt;</a></li>
+    <li class="page-item" ><a class="page-link" href="#">&gt;&gt;</a></li>`;
+
+    document.querySelector(".pagination").innerHTML = paginationHTML; 
+};
+
+
+const moveToPage = async (pageNum) => {
+    console.log("m",pageNum);
+    page = pageNum;
+    await getLatestNews();
+}
 
 //에러 경고창을 보여줌.
 const errorRender = (errorMessage) => {
